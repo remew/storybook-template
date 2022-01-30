4a259daff7d39c8e8293f165fe73faba0b1dc533
remew <foolishremew@gmail.com>
2022-01-30 19:08:03 +0900

    add config for base url

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,5 +10,11 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "storybook-builder-vite"
-  }
+  },
+  async viteFinal(config, { configType }) {
+    return {
+      ...config,
+      base: './',
+    };
+  },
 }
